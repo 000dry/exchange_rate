cacd7991535245df51a5a74b672665a7bda7be35
Joe Ryan <joeryansaccount@googlemail.com>
2018-06-24 20:55:45 +0100

    Fixing bugs

--- lib/exchange_rate/cli.rb
+++ lib/exchange_rate/cli.rb
@@ -12,14 +12,14 @@ module ExchangeRate
     desc "rate DATE CURRENCY", "Retrieves the rate of a single currency on a specified date"
     def rate(date, currency)
       rate = ExchangeRate.rate(date, currency)
-      puts = "#{date}, #{currency}: #{rate}"
+      puts = "Rate #{currency}, #{date}: #{rate}"
     end
 
     desc "calculate DATE BASE COUNTER",
     "Calculates the exchange rates of two given currencies on a specified date"
     def calculate(date, base, counter)
       rate = ExchangeRate.at(date, base, counter)
-      puts "Exchange rate for #{base} / #{counter} #{date}: #{rate}"
+      puts "Exchange rate #{base} / #{counter}, #{date}: #{rate}"
     end
   end
 end
